ed0133207a38ac1bdba0fa2b1fe903ab116e15ec
y_uuki <yuki.tsubo@gmail.com>
2017-05-16 16:36:19 +0900

    Add mackerelio/checkers dependency

---
+++ vendor/github.com/mackerelio/checkers/checkers.go
@@ -0,0 +1,76 @@
+// Package checkers is utility for checker commands
+package checkers
+
+import (
+	"fmt"
+	"os"
+)
+
+// Status declare the result of the checker command
+type Status int
+
+// Statuses
+const (
+	OK Status = iota
+	WARNING
+	CRITICAL
+	UNKNOWN
+)
+
+func (st Status) String() string {
+	switch {
+	case st == 0:
+		return "OK"
+	case st == 1:
+		return "WARNING"
+	case st == 2:
+		return "CRITICAL"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+// NewChecker returns new Checker
+func NewChecker(st Status, msg string) *Checker {
+	return &Checker{
+		Status:  st,
+		Message: msg,
+	}
+}
+
+// Checker is utility struct for check script
+type Checker struct {
+	Name    string
+	Status  Status
+	Message string
+}
+
+// Exit with message
+func (ckr *Checker) Exit() {
+	fmt.Println(ckr.String())
+	os.Exit(int(ckr.Status))
+}
+
+func (ckr *Checker) String() string {
+	return fmt.Sprintf("%s %s: %s", ckr.Name, ckr.Status, ckr.Message)
+}
+
+// Ok exiting with OK status
+func Ok(msg string) *Checker {
+	return NewChecker(OK, msg)
+}
+
+// Warning exiting with WARNING status
+func Warning(msg string) *Checker {
+	return NewChecker(WARNING, msg)
+}
+
+// Critical exiting with CRITICAL status
+func Critical(msg string) *Checker {
+	return NewChecker(CRITICAL, msg)
+}
+
+// Unknown exiting with UNKNOWN status
+func Unknown(msg string) *Checker {
+	return NewChecker(UNKNOWN, msg)
+}
---
+++ vendor/github.com/mackerelio/checkers/checkers_test.go
@@ -0,0 +1,63 @@
+package checkers
+
+import "testing"
+
+func TestOk(t *testing.T) {
+	ckr := Ok("OK!")
+
+	if ckr.Status != OK {
+		t.Errorf("ckr.Status should be OK but:%s\n", ckr.Status)
+	}
+	if ckr.Message != "OK!" {
+		t.Errorf("ckr.Message should be 'OK!' but:%s\n", ckr.Message)
+	}
+
+	if ckr.String() != " OK: OK!" {
+		t.Errorf("ckr.Status should be 'OK OK!' but:%s\n", ckr.String())
+	}
+}
+
+func TestWarning(t *testing.T) {
+	ckr := Warning("warn!")
+
+	if ckr.Status != WARNING {
+		t.Errorf("ckr.Status should be WARNING but:%s\n", ckr.Status)
+	}
+	if ckr.Message != "warn!" {
+		t.Errorf("ckr.Message should be 'warn!' but:%s\n", ckr.Message)
+	}
+
+	if ckr.String() != " WARNING: warn!" {
+		t.Errorf("ckr.Status should be 'WARNING warn!' but:%s\n", ckr.String())
+	}
+}
+
+func TestCritical(t *testing.T) {
+	ckr := Critical("crit!")
+
+	if ckr.Status != CRITICAL {
+		t.Errorf("ckr.Status should be CRITICAL but:%s\n", ckr.Status)
+	}
+	if ckr.Message != "crit!" {
+		t.Errorf("ckr.Message should be 'crit!' but:%s\n", ckr.Message)
+	}
+
+	if ckr.String() != " CRITICAL: crit!" {
+		t.Errorf("ckr.Status should be 'CRITICAL crit!' but:%s\n", ckr.String())
+	}
+}
+
+func TestUnknown(t *testing.T) {
+	ckr := Unknown("unknown!")
+
+	if ckr.Status != UNKNOWN {
+		t.Errorf("ckr.Status should be UNKNOWN but:%s\n", ckr.Status)
+	}
+	if ckr.Message != "unknown!" {
+		t.Errorf("ckr.Message should be 'unknown!' but:%s\n", ckr.Message)
+	}
+
+	if ckr.String() != " UNKNOWN: unknown!" {
+		t.Errorf("ckr.Status should be 'UNKNOWN unknown!' but:%s\n", ckr.String())
+	}
+}
