a9c8a896b4c9d98979fa5cd32b8a0413b15ed02c
y_uuki <yuki.tsubo@gmail.com>
2017-05-16 17:03:50 +0900

    Implementation

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mackerelio/checkers"
+	"github.com/mackerelio/mackerel-agent/checks"
+	"github.com/mackerelio/mackerel-agent/command"
+	"github.com/mackerelio/mackerel-agent/config"
+)
+
+var statusToExitCode = map[checks.Status]checkers.Status{
+	checks.StatusOK:       checkers.OK,
+	checks.StatusWarning:  checkers.WARNING,
+	checks.StatusCritical: checkers.CRITICAL,
+	checks.StatusUnknown:  checkers.UNKNOWN,
+}
+
+func run(args []string) int {
+	var (
+		conffile string
+	)
+	flags := flag.NewFlagSet("mkr-check", flag.ContinueOnError)
+	flags.StringVar(&conffile, "conffile", "/etc/mackerel-agent/mackerel-agent.conf", "mackerel-agent's config file")
+
+	if err := flags.Parse(args); err != nil {
+		log.Println(err)
+		return 10
+	}
+
+	conf, err := config.LoadConfig(conffile)
+	if err != nil {
+		log.Println(err)
+		return 11
+	}
+
+	agent := command.NewAgent(conf)
+	for _, c := range agent.Checkers {
+		report := c.Check()
+		// log.Print(report.Message)
+
+		var exitCode checkers.Status
+		if code, ok := statusToExitCode[report.Status]; ok {
+			exitCode = code
+		}
+		ckr := checkers.NewChecker(exitCode, report.Message)
+		fmt.Print(ckr.String())
+		if report.Status != checks.StatusOK {
+			return int(ckr.Status)
+		}
+	}
+
+	return int(checkers.OK)
+}
+
+func main() {
+	os.Exit(run(os.Args[1:]))
+}
